3b5f7509c48717bb936270677706903913920f8d
eveliinahampus <t2haev00@students.oamk.fi>
2024-11-23 16:09:14 +0200

    Deploy debug

--- app.py
+++ app.py
@@ -1,38 +1,50 @@
-from dash import Dash, dcc, callback, Output, Input
+from dash import Dash, dcc, html, callback, Output, Input
 import plotly.express as px
 import pandas as pd
+from datetime import datetime
 
-df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/refs/heads/master/MTA_Ridership_by_DATA_NY_GOV.csv")
+df = pd.read_csv("MTA_Ridership_by_DATA_NY_GOV.csv")
 
-# Create a dash application
+# Create a Dash application
 app = Dash(__name__)
 server = app.server
 
-app.layout = [
-    dcc.Dropdown(options=[{'label':'Subway %', 'value':'Subways: % of Comparable Pre-Pandemic Day'},
-                          {'label':'Bus %', 'value': 'Buses: % of Comparable Pre-Pandemic Day'}],
-                 value='Subways: % of Comparable Pre-Pandemic Day',
-                 clearable=False,
-                 id='column-selected'
-                 ),
+# App layout
+app.layout = html.Div([
+    dcc.Dropdown(
+        options=[
+            {'label': 'Subway %', 'value': 'Subways: % of Comparable Pre-Pandemic Day'},
+            {'label': 'Bus %', 'value': 'Buses: % of Comparable Pre-Pandemic Day'}
+        ],
+        value='Subways: % of Comparable Pre-Pandemic Day',
+        clearable=False,
+        id='column-selected'
+    ),
     dcc.Graph(id='my-graph')
-]
+])
 
+# Callback to update the graph
 @callback(
-    Output('my-graph','figure'),
+    Output('my-graph', 'figure'),
     Input('column-selected', 'value')
 )
 def update_graph(col_slctd):
+    # Convert the "03/11/2020" string to a datetime object
+    pandemic_date = datetime.strptime("03/11/2020", "%m/%d/%Y")
+
     fig = px.bar(df, x="Date", y=col_slctd)
     fig.update_layout(xaxis_title=None)
     fig.update_traces(marker_color="green")
-    fig.add_vline(x="03/11/2020", line_width=1, line_dash="dash", line_color="blue")
-    fig.add_annotation(x="03/11/2020", y=100,
-                       text="<--- World Health Organization declares a global COVID-19 pandemic.",
-                       showarrow=False,
-                       xshift=220)
+    fig.add_vline(x=pandemic_date, line_width=1, line_dash="dash", line_color="blue")
+    fig.add_annotation(
+        x=pandemic_date, 
+        y=100, 
+        text="<--- World Health Organization declares a global COVID-19 pandemic.",
+        showarrow=False,
+        xshift=220
+    )
     return fig
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0')
